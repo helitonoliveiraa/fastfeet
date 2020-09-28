7a8d3dead9537fe96e6e655ff2ea30c0b0ce039a
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-09-28 11:32:14 -0300

    :wrench: chore: configure private routes and proptypes validator

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
-import SignIn from '~/pages/SignIn';
+import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
